46937c3237b5fc6f15a20a673c9f49bc31e0862c
Estevo U. C. Castro <euccastro@gmail.com>
2020-08-20 02:30:46 +0200

    auth (routing part; hardcoded username/password)
    Next step would be hashing and persisting passwords.

--- src/clj/relembra/ring_handler.clj
+++ src/clj/relembra/ring_handler.clj
@@ -1,15 +1,19 @@
 (ns relembra.ring-handler
-  (:require [hiccup.core :refer (html)]
-            [hiccup.page :refer (doctype)]
+  (:require [buddy.auth :refer [authenticated?]]
+            [buddy.auth.accessrules :refer [restrict]]
+            [buddy.auth.backends.session :refer [session-backend]]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [doctype]]
             [integrant.core :as ig]
             [muuntaja.middleware :refer [wrap-format]]
             [reitit.ring :as rring]
-            [ring.middleware.anti-forgery :refer (*anti-forgery-token*
-                                                  wrap-anti-forgery)]
-            [ring.middleware.params :refer (wrap-params)]
-            [ring.middleware.keyword-params :refer (wrap-keyword-params)]
-            [ring.middleware.session :refer (wrap-session)]
-            [ring.util.http-response :refer (content-type ok)]))
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*
+                                                  wrap-anti-forgery]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.session :refer [wrap-session]]
+            [ring.util.http-response :refer [content-type ok see-other]]))
 
 
 (defn- html5-ok [title body]
@@ -37,10 +41,59 @@
                    "\";")]
              [:script {:type "text/javascript"
                        :src "/js/relembra.js"}]]))
+
+(defn- login [redirect-to]
+  (html5-ok "Login"
+   [:body
+    [:form {:method "post"
+            :action "/login"}
+     [:input {:type "hidden" :name "csrf-token" :value *anti-forgery-token*}]
+     [:input {:type "hidden" :name "redirect-to" :value redirect-to}]
+     [:div
+      [:label "Name"
+       [:input {:type "text" :name "name" :required true}]]]
+     [:div
+      [:label "Password"
+       [:input {:type "password" :name "password" :required true}]]]
+     [:div
+      [:input {:type "submit" :value "Login"}]]]]))
+
+
+(defn on-error [request _]
+  (->
+   {:status 403
+    :body (str "Access to " (:uri request) " is not authorized")}
+   (content-type "text/plain; charset=utf-8")))
+
+
+(defn wrap-restricted [handler]
+  (restrict handler {:handler authenticated?
+                     :on-error on-error}))
+
+
+(defn wrap-auth [handler]
+  (let [backend (session-backend)]
+    (-> handler
+        (wrap-authentication backend)
+        (wrap-authorization backend))))
+
+
+(defn handle-login [{{:keys [name password redirect-to]} :params
+                     :as req}]
+  (if (= [name password] ["es" "quod"])
+    (-> (see-other redirect-to)
+        (assoc :session (assoc (:session req) :identity "es")))
+    ;; XXX: set error string in login instead
+    (on-error {:uri redirect-to} nil)))
+
+
 (defn- handler [crux-node]
   (rring/ring-handler
    (rring/router
-    [["/test" {:get (constantly {:status 200
+    [["/login" {:get #(-> % :params :redirect-to login)
+                :post handle-login}]
+     ["/test" {:middleware [wrap-restricted]
+               :get (constantly {:status 200
                                  :headers {"Content-Type" "text/plain"}
                                  :body "OK"})}]])
    (rring/routes
@@ -53,9 +106,10 @@
 (defmethod ig/init-key ::handler
   [_ {:keys [crux-node]}]
   (-> (handler crux-node)
+      wrap-auth
       (wrap-anti-forgery
        {:read-token (fn [req]
-                      (get-in req [:body-params :csrf-token]))})
+                      (get-in req [:params :csrf-token]))})
       wrap-session
       wrap-format
       wrap-keyword-params
