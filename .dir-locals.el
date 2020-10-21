616980f57368220c516158885f1c152489078c04
Estevo U. Caamaño Castro <euccastro@gmail.com>
2020-10-21 23:26:34 +0200

    enable , t a in spacemacs+cider

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,6 +5,6 @@
   (cider-default-cljs-repl . figwheel-main)
   (cider-figwheel-main-default-options . "dev"))
  (clojure-mode
-  (cider-clojure-cli-global-options . "-A:dev:hotload-deps"))
+  (cider-clojure-cli-global-options . "-A:dev:hotload-deps:test"))
  (clojurescript-mode
   (cider-clojure-cli-global-options . "-A:dev")))
